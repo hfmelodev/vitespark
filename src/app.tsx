3838b202c1210a4eebbaa108b201b62f1a2f120a
Hilquias Ferreira Melo <hilquiasfmelo@gmail.com>
2025-01-12 13:58:01 -0300

    Downgrade versao types/react para a 18

--- src/app.tsx
+++ src/app.tsx
@@ -1,17 +1,13 @@
-import { useState } from 'react'
 import { Button } from './components/ui/button'
 
 export function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="flex min-h-screen flex-col items-center justify-center">
-      <Button type="button" onClick={() => setCount(count => count + 1)}>
-        count is {count}
-      </Button>
       <p>
         Edit <code>src/app.tsx</code> and save
       </p>
+
+      <Button>Hello World</Button>
     </div>
   )
 }
