da13ebc39bff56699821ce9dcc844961a91fe391
Hilquias Ferreira Melo <hilquiasfmelo@gmail.com>
2025-04-19 14:21:35 -0300

    configuração inicial de rotas, temas e not-found

--- src/app.tsx
+++ src/app.tsx
@@ -1,25 +1,16 @@
-import { Route, Routes } from 'react-router'
-import { AppLayout } from './_layouts/app'
-import { AuthLayout } from './_layouts/auth'
-import { NotFound } from './not-found'
-import { Auth } from './pages/auth'
-import { Home } from './pages/home'
+import { Helmet, HelmetProvider } from 'react-helmet-async'
+import { BrowserRouter } from 'react-router'
+import { Toaster } from 'sonner'
+import { Routes } from './routes'
 
 export function App() {
   return (
-    <Routes>
-      <Route element={<AppLayout />}>
-        <Route index element={<Home />} />
-        <Route path="mypage" element={<h1>MyPage</h1>} />
-      </Route>
-
-      <Route path="auth" element={<AuthLayout />}>
-        <Route index element={<Auth />} />
-        <Route path="login" element={<h1>Login</h1>} />
-      </Route>
-
-      {/* Rota para páginas não encontradas */}
-      <Route path="*" element={<NotFound />} />
-    </Routes>
+    <HelmetProvider>
+      <Helmet titleTemplate="%s | Crypton" />
+      <Toaster position="top-center" richColors />
+      <BrowserRouter>
+        <Routes />
+      </BrowserRouter>
+    </HelmetProvider>
   )
 }
