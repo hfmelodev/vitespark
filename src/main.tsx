aac9849b80bbc11375395b7b094a50df41234ce4
Hilquias Ferreira Melo <hilquiasfmelo@gmail.com>
2025-04-27 19:14:39 -0300

    ajuste nas rotas e removido renovate

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router'
 
-import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { App } from './app'
 import './global.css'
 
 createRoot(document.getElementById('root')!).render(
-  <HelmetProvider>
-    <StrictMode>
-      <Helmet titleTemplate="%s | ViteSpark" />
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </StrictMode>
-  </HelmetProvider>
+  <StrictMode>
+    <App />
+  </StrictMode>
 )
---
+++ src/router.tsx
@@ -0,0 +1,25 @@
+import { Route, Routes } from 'react-router'
+import { AppLayout } from './_layouts/app'
+import { AuthLayout } from './_layouts/auth'
+import { NotFound } from './not-found'
+import { Auth } from './pages/auth'
+import { Home } from './pages/home'
+
+export function Router() {
+  return (
+    <Routes>
+      <Route element={<AppLayout />}>
+        <Route index element={<Home />} />
+        <Route path="mypage" element={<h1>MyPage</h1>} />
+      </Route>
+
+      <Route path="auth" element={<AuthLayout />}>
+        <Route index element={<Auth />} />
+        <Route path="login" element={<h1>Login</h1>} />
+      </Route>
+
+      {/* Rota para páginas não encontradas */}
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  )
+}
